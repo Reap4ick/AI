56d7258a2a51a1eb12e943dd7a8aabb34f007112
Reap4ick <122297692+Reap4ick@users.noreply.github.com>
2025-04-14 22:23:24 +0300

    first commit

---
+++ 1st/ai1.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+# Завантаження даних
+df = pd.read_csv("cars_plus.csv")
+print(df.head())
+
+# Вибір ознак та цільової змінної//// year,engine_volume,mileage,horsepower,price
+X = df[['brand','model','year','engine_volume','mileage','horsepower']] # features
+y = df['price']                                  # target
+
+print("Features: ", X)
+print("Target:", y)
+
+# Розділення на train/test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        # Для числових ознак ('year', 'engine_volume', 'mileage', 'horsepower') нічого не змінюємо ('passthrough')
+        ('num', 'passthrough', ['year', 'engine_volume', 'mileage', 'horsepower']),
+        # Для категоріальних ознак ('brand', 'model') застосовуємо OneHotEncoder
+        ('cat', OneHotEncoder(handle_unknown='ignore'), ['brand', 'model'])
+    ])
+
+
+# Створення та навчання моделі
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('regressor', LinearRegression())
+])
+model.fit(X_train, y_train)
+
+# Прогноз та оцінка
+# your_apartment = pd.DataFrame([{
+#     'area': 81,           # площа в м²
+#     'rooms': 2,           # кількість кімнат
+#     'floor': 5,           # поверх
+#     'year_built': 1990    # рік будівництва
+# }])  2006,2.6,37,325,353978.54
+
+# your_apartment = pd.DataFrame([{
+#     'year': 2006,
+#     'engine_volume':2.6,
+#     'mileage': 325,
+#     'horsepower':353978.54,
+# }])
+
+your_car = pd.DataFrame([{
+    'brand': 'BMW',          # Приклад бренду
+    'model': 'M3',           # Приклад моделі
+    'year': 2006,
+    'engine_volume': 2.6,
+    'mileage': 37,
+    'horsepower': 325
+}])
+
+# Прогноз ціни
+predicted_price = model.predict(your_car)
+print(f"Прогнозована ціна Машини: {predicted_price[0]:,.2f} $")
+
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mape = np.mean(np.abs((y_test - y_pred) / y_test)) * 100
+print(f"MAPE: {mape:.2f}%")
+
+# Візуалізація: справжні ціни vs прогноз
+plt.scatter(y_test, y_pred)
+plt.xlabel("Справжня ціна")
+plt.ylabel("Прогнозована ціна")
+plt.title("Справжня vs Прогнозована ціна")
+plt.plot([y.min(), y.max()], [y.min(), y.max()], color='red')
+plt.show()
